b4a58435535329154fb3a24d684b995239b0a2dc
Jeffrey Richardson <jeff.dsv@gmail.com>
2025-02-25 19:56:00 -0400

    Adicionando Estrutura inicial

---
+++ src/test/java/com/Library_API/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Library_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
